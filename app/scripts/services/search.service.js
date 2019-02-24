7760fcb44d2dbf2ffa5adbffb55d9529ec6707b6
Santiago Alurralde <santialur@192-168-1-113.tpgi.com.au>
2019-02-25 02:45:24 +1100

    Refactor

--- app/scripts/services/search.service.js
+++ app/scripts/services/search.service.js
@@ -13,70 +13,66 @@ angular.module('deputyApp')
     this.getUseCaseList = function(opt) {
 	    return $http.get('assets/json/data.json')
 	    .then(function(response) {
-
-	    	//Items after filter
-	    	let filteredItems = [];
-
-	    	let pageLength = opt.pagination.pageLength;	
-	    	let currentPage = opt.pagination.currentPage;
-
 	    	//Items fetched
 	    	let items = response.data;
 
+	    	//Category Setting
         	for(let cat in categories) {
 		    	categories[cat] = getValuesOfCategory(cat, items);
 		    }
 
-	    	let filters = opt.filter.applied;
+	    	//Items after filter
+	    	let filteredItems = [];
+	    	let query = opt.filter.applied;
 
     		filteredItems = items;
-    		console.log('Filtered Items ', filteredItems);
 
-	    	if (opt.filter.amount !== 0) {
-	    		console.log('Filters', filters);
-
-	    		for (let prop in filters) {
-	    			console.log(prop, ' ', filters[prop]);
-
-	    			if (filters[prop].length) { //This criteria has been used for filtering
+	    	if (opt.filter.amount !== 0) { //Filters applied
+	    		for (let prop in query) {
+	    			if (query[prop].length) { //This criteria has been used for filtering
 	    				filteredItems = _.filter(filteredItems, (item) => {
 							return Array.isArray(item[prop]) ? 
-								_.intersection(item[prop], filters[prop]).length > 0 : 
-								_.includes(filters[prop], item[prop]);
+								_.intersection(item[prop], query[prop]).length > 0 : 
+								_.includes(query[prop], item[prop]);
 			    		});
 	    			}
 	    		}
 	    	} 
-    		console.log('filtered items', filteredItems, filteredItems.length);
+
+	    	//Pagination Settings
+			let pageLength = opt.pagination.pageLength;	
+	    	let currentPage = opt.pagination.currentPage;
 
 	    	let end = currentPage * pageLength;
 	    	let start = end - pageLength;
 
-    	    response.pagination = {
-    	    	totalItems: filteredItems.length,
-    	    	currentPage: currentPage,
-    	    	pageLength: pageLength
-    	    };
-
-    	    response.data = filteredItems.slice(start, end);
-
-    	    response.categories = categories;
+	    	response = {
+	    		pagination: {
+	    	    	totalItems: filteredItems.length,
+	    	    	currentPage: currentPage,
+	    	    	pageLength: pageLength
+	    	    },
+	    	    data: filteredItems.slice(start, end),
+	    	    categories: categories
+	    	}
 
     		return response;
     	});
     };
 
 	function getValuesOfCategory(cat, collection) {
-    	let values = [];
-    	let count = [];
+    	let values = [], 
+    		count = [];
+
     	collection.forEach(item => values = values.concat(item[cat]));	
     	count = _.countBy(values);
     	values = _.uniq(values);
 
-    	console.log('Count', count, 'values', values);
     	values.forEach((item, index) => {
-    		console.log('CONSOLE', item, index);
-    		values[index] = { id: item, label: item + ' (' + count[item] + ')' };
+    		values[index] = { 
+    			id: item, 
+    			label: `${item} (${count[item]})` 
+    		};
     	});	
 
     	return values;
